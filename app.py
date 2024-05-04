2e81b162fb6fd0d52415afea302aaeee82ee8442
Javier Alarcon <javieralarconbusiness@outlook.com>
2024-05-04 13:12:54 -0700

    Started flask app

--- app.py
+++ app.py
@@ -1,30 +1,50 @@
 # Import the dependencies.
-
-
+from flask import Flask, jsonify
+import datetime as dt
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import Session
+from sqlalchemy.ext.automap import automap_base
 
 #################################################
 # Database Setup
 #################################################
 
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
-
-
+app = Flask(__name__)
 
 #################################################
 # Flask Routes
 #################################################
+
+
+@app.route("/")
+def home():
+    
+    '''List all available api routes'''
+    return ("""
+    <h1>Climate App</h1>
+    <h3>Available Routes:</h3>
+    <ul>
+        <li><a href="/api/v1.0/precipitation">/api/v1.0/precipitation</a></li>
+        <li><a href="/api/v1.0/stations">/api/v1.0/stations</a></li>
+        <li><a href="/api/v1.0/tobs">/api/v1.0/tobs</a></li>
+        <li>/api/v1.0/&lt;start&gt;</li>
+        <li>/api/v1.0/&lt;start&gt;/&lt;end&gt;</li>
+    </ul>
+    """)
+    
